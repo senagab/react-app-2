c7aa30c34b2628fd67f626bbc6aa21454230d34d
senagab <senagab@gmail.com>
2025-02-16 00:07:51 -0400

    feat: first components, props and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react"
+import { Tweet } from "./components/Tweet"
+
 function App() {
+  const [tweets, setTweets] = useState<string[]>([
+    'Tweet 1',
+    'Tweet 2',
+    'Tweet 3',
+    'Tweet 4',
+  ])
+
+  function createTweet() {
+    setTweets([...tweets, 'Tweet 5'])
+  }
 
   return (
-    <h1>Hello World</h1>
+      <div>
+        {tweets.map(tweet => {
+          return <Tweet text={tweet} />
+        })}
+
+        <button onClick={createTweet}>Adicionar tweet</button>
+      </div>
   )
 }
 
